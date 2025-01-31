291f9c58e734ac31aca8bbea8b759f7446f54d56
Grzegorz Pawłowski (GPW) <gpw@kmd.dk>
2025-01-31 15:13:51 +0100

    add helper class to test data

---
+++ VSCodeExtension/test-data/tests/Application.Tests/FooHelper.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace test-data.tests.Application.Tests
+{
+    public class FooHelper
+    {
+        
+    }
+}
