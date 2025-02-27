90543a6a081cad4d7d6d4c0bbcffe567435b186e
Grzegorz Pawłowski (GPW) <gpw@kmd.dk>
2025-02-27 17:40:37 +0100

    do not cover tests

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.ts'],
-  collectCoverage: true,
+  collectCoverage: false,
   collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov'],
